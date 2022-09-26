b1fc3b01c9aab62c7d3b60507b56d793063d99a7
hyrious <hyrious@outlook.com>
2022-09-26 13:24:10 +0800

    beatify trace test

--- test/trace.ts
+++ test/trace.ts
@@ -1,3 +1,33 @@
 import { match_trace } from "../src/trace";
 
-console.log(match_trace("th", "tth-hash"));
+function test(pattern: string, str: string) {
+  const match = match_trace(pattern, str);
+  if (match) {
+    const parts: { bold?: true; text: string }[] = [];
+    let last = 0;
+    match.stops.forEach((i) => {
+      if (last < i) {
+        parts.push({ text: str.slice(last, i) });
+      }
+      parts.push({ bold: true, text: str[i] });
+      last = i + 1;
+    });
+    if (last < str.length) {
+      parts.push({ text: str.slice(last) });
+    }
+
+    let output = "";
+    parts.forEach(({ text, bold }) => {
+      if (bold) {
+        output += "\x1b[1m";
+      }
+      output += text;
+      if (bold) {
+        output += "\x1b[22m";
+      }
+    });
+    console.log(match.score, output);
+  }
+}
+
+test("th", "tth-hash");
